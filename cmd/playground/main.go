da9dd96218f6adb3cdee2a203dd4c17d5340a52f
mi-wada <mtak.wd@gmail.com>
2025-01-05 17:42:46 +0900

    chapter 2

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"compress/gzip"
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	// cmd+p -> "Start Debugging"
-	fmt.Println("Hello, world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Encoding", "gzip")
+		w.Header().Set("Content-Type", "application/json")
+		src := map[string]string{
+			"hello": "world",
+		}
+		srcJSON, _ := json.Marshal(src)
+
+		gzipWriter := gzip.NewWriter(w)
+		defer gzipWriter.Close()
+
+		multiWriter := io.MultiWriter(log.Writer(), gzipWriter)
+		multiWriter.Write(srcJSON)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
