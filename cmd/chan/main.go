40262b0ebcddaad84ec6574df829be6cc1d9c657
mi-wada <mtak.wd@gmail.com>
2025-01-06 15:53:05 +0900

    chapter 5

--- cmd/chan/main.go
+++ cmd/chan/main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
+	start := time.Now()
 	fmt.Printf("time.Now(): %v\n", time.Now())
-	<-time.After(10 * time.Second)
+	<-time.After(2 * time.Second)
+	end := time.Now()
+	duration := end.Sub(start)
+	fmt.Printf("duration.String(): %v\n", duration.String())
 	fmt.Printf("time.Now(): %v\n", time.Now())
 }
