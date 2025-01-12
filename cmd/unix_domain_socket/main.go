cd23055e234982a5adebae528adf2256266b2251
mi-wada <mtak.wd@gmail.com>
2025-01-12 10:09:57 +0900

    chapter 8

---
+++ cmd/unix_domain_socket/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("Must specify s or c")
+	}
+	switch os.Args[1] {
+	case "s":
+		server()
+	case "c":
+		client()
+	default:
+		panic(fmt.Sprintf("Unknown flag: %s", os.Args[1]))
+	}
+}
+
+func server() {
+	listener, err := net.Listen("unix", "socketfile")
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			panic(err)
+		}
+		conn.Write([]byte("Hello"))
+
+		buf := make([]byte, 5)
+		conn.Read(buf)
+		if string(buf) == "World" {
+			log.Println("World received")
+		} else {
+			log.Panicf("Unexpected message: %s", string(buf))
+		}
+
+		conn.Write([]byte("Bye"))
+		conn.Close()
+	}
+}
+
+func client() {
+	conn, err := net.Dial("unix", "socketfile")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 5)
+	conn.Read(buf)
+	if string(buf) == "Hello" {
+		log.Println("Hello received")
+	} else {
+		log.Panicf("Unexpected message: %s", string(buf))
+	}
+
+	conn.Write([]byte("World"))
+
+	buf = make([]byte, 3)
+	conn.Read(buf)
+	if string(buf) == "Bye" {
+		log.Println("Bye received")
+	} else {
+		log.Panicf("Unexpected message: %s", string(buf))
+	}
+}
