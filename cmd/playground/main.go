42a8ba630424af791284ffc178871bf7f4411ec8
mi-wada <mtak.wd@gmail.com>
2025-01-18 11:14:45 +0900

    update deps

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -1,28 +1,12 @@
 package main
 
-import (
-	"compress/gzip"
-	"encoding/json"
-	"io"
-	"log"
-	"net/http"
-)
+import "fmt"
+
+func a() *string {
+	s := "hogehoge"
+	return &s
+}
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Encoding", "gzip")
-		w.Header().Set("Content-Type", "application/json")
-		src := map[string]string{
-			"hello": "world",
-		}
-		srcJSON, _ := json.Marshal(src)
-
-		gzipWriter := gzip.NewWriter(w)
-		defer gzipWriter.Close()
-
-		multiWriter := io.MultiWriter(log.Writer(), gzipWriter)
-		multiWriter.Write(srcJSON)
-	})
-
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("a(): %v\n", a())
 }
